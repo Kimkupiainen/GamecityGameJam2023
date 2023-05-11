d4270f07c8379f258875381e3883020b5097f4b8
QAK1mpp4 <kim.kupiainen@criticalforce.fi>
2023-05-11 23:02:10 +0300

    ai perhana, netti poikki

--- Assets/Scripts/BallMaxVelocity.cs
+++ Assets/Scripts/BallMaxVelocity.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 
 public class BallMaxVelocity : MonoBehaviour
 {
-    Rigidbody2D rb;
-    int maxvelocity = 40;
+    public Rigidbody2D rb;
+    public float launchForce = 10f;    
+    public float velocityDamping = 0.95f;
+    public float gravityScale = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        LaunchBall();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (rb.velocity <= maxvelocity)
+        // Apply custom gravity scale
+        rb.gravityScale = gravityScale;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
         {
+            Vector2 direction = collision.GetContact(0).normal;
+            Vector2 launchVelocity = direction * launchForce;
 
+            rb.velocity = launchVelocity;
         }
+        rb.velocity *= velocityDamping;
+    }
+    private void LaunchBall()
+    {
+        Vector2 randomDirection = Random.insideUnitCircle.normalized;
+        float angle = Vector2.Angle(Vector2.up, randomDirection);
+
+        if (angle < 110f)
+        {
+            randomDirection = Quaternion.Euler(0f, 0f, 110f) * Vector2.up;
+        }
+
+        Vector2 launchVelocity = randomDirection * launchForce;
+
+        rb.velocity = -launchVelocity;
     }
 }
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using Cinemachine;
+using System.Collections.Generic;
+
+public class CameraController : MonoBehaviour
+{
+    public List<Transform> gameObjects;
+
+    public Vector3 offset;
+    private Vector3 velocity;
+    public float smoothtime = 0.5f;
+    public float minzoom = 61.4f;
+    public float maxzoom = 30f;
+
+    void LateUpdate()
+    {
+        if (gameObjects.Count == 0)
+        {
+            return;
+        }
+        Move();
+        Zoomiers();
+    }
+    void Move()
+    {
+        Vector3 centerpoint = Getcenterpoint();
+        Vector3 newposition = centerpoint + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, newposition, ref velocity, smoothtime);
+    }
+    void Zoomiers()
+    {
+        float newZoom = Mathf.Lerp(maxzoom, minzoom, Getgreatestdistance()/50f);
+    }
+    float Getgreatestdistance()
+    {
+        var bounds = new Bounds(gameObjects[0].position, Vector3.zero);
+        for (int i = 0; i < gameObjects.Count; i++)
+        {
+            bounds.Encapsulate(gameObjects[i].position);
+        }
+        return bounds.size.x;
+    }
+
+    Vector3 Getcenterpoint()
+    {
+        if (gameObjects.Count == 1)
+        {
+            return gameObjects[0].position;
+        }
+
+        var bounds = new Bounds(gameObjects[0].position, Vector3.zero);
+        for (int i = 0; i<gameObjects.Count; i++)
+        {
+            bounds.Encapsulate(gameObjects[i].position);
+        }
+        return bounds.center;
+    }
+
+}
