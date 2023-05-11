75bdc4959f8599ad4b8e1e4f5c6cf1946014aeec
QAK1mpp4 <kim.kupiainen@criticalforce.fi>
2023-05-11 21:04:32 +0300

    säädän pallon velocityä

---
+++ Assets/Scripts/BallMaxVelocity.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMaxVelocity : MonoBehaviour
+{
+    Rigidbody2D rb;
+    int maxvelocity = 40;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity <= maxvelocity)
+        {
+
+        }
+    }
+}
